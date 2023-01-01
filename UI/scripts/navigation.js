76392d6e25d2af4426c022eaee9b431d9d629cbf
Petr Šatka <sat@anet.eu>
2023-01-01 15:12:38 +0100

    Vynechání právě zobrazené položky z menu

--- UI/scripts/navigation.js
+++ UI/scripts/navigation.js
@@ -1,7 +1,10 @@
 import { menuContent } from './menu-content.js';
 
 (function createNavigation() {
-  var menuItemString = menuContent.map(itm => `<a href="${itm.link}">${itm.title}</a>`).join('');
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  page = page.substring(0, page.indexOf(".html") + 5);
+  var menuItemString = menuContent.filter(itm => itm.link.indexOf(page) < 0).map(itm => `<a href="${itm.link}">${itm.title}</a>`).join('');
   $(".main-header").append(`
   <nav id="hamnav">
     <label for="hamburger">&#9776;</label>
