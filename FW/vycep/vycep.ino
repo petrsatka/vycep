59d2dc6bd633e6bc9c28f04067528f147fd30a84
Petr Šatka <sat@anet.eu>
2023-02-23 21:15:45 +0100

    Příprava serveru

--- FW/vycep/vycep.ino
+++ FW/vycep/vycep.ino
@@ -8,19 +8,20 @@ AsyncWebServer server(80);
 SemaphoreHandle_t xSemaphore = xSemaphoreCreateMutex();
 User user(xSemaphore);
 
-AsyncStaticWebHandler *firstRegistrationHandler = NULL;
+AsyncCallbackWebHandler *firstRegistrationRedirectHandler = NULL;
+AsyncStaticWebHandler *firstRegistrationStaticHandler = NULL;
 
 void onNotFound(AsyncWebServerRequest *request) {
   request->send(404);
 }
 
-bool filterNotLoggetIn(AsyncWebServerRequest *request) {
-  int headers = request->headers();
-  int i;
-  for (i = 0; i < headers; i++) {
-    AsyncWebHeader *h = request->getHeader(i);
-    Serial.printf("HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
-  }
+bool isAuthenticatedFilter(AsyncWebServerRequest *request) {
+  Serial.println("IsAuthenticatedFilter");
+  return true;
+}
+
+bool isAuthorizedAdminFilter(AsyncWebServerRequest *request) {
+  Serial.println("IsAdminFilter");
   return true;
 }
 
@@ -34,36 +35,44 @@ void serverInit() {
   server.serveStatic("/registration.html", LittleFS, "/www/registration.html");
 
   //Pouze pokud nejsou uživatelé
-  //DOPLNIT IF !!!
-  firstRegistrationHandler = &server.serveStatic("/first-registration.html", LittleFS, "/www/first-registration.html");
+  if (!user.isUserSet()) {
+    firstRegistrationRedirectHandler = &server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+      request->redirect("/first-registration.html");
+    });
 
-  //Po registraci odstranit handler
-  //server.removeHandler(firstRegistrationHandler);
-  //firstRegistrationHandler = NULL;
+    firstRegistrationStaticHandler = &server.serveStatic("/first-registration.html", LittleFS, "/www/first-registration.html");
 
-  //Přihlášený uživatel
-  server.serveStatic("/orders.html", LittleFS, "/www/orders.html");
-  server.serveStatic("/password-change.html", LittleFS, "/www/password-change.html");
-
-  //Přihlášený s právy
-  server.serveStatic("/paymen.html", LittleFS, "/www/paymen.html");
-
-  //Admin
-  server.serveStatic("/users.html", LittleFS, "/www/users.html");
-  server.serveStatic("/settings.html", LittleFS, "/www/settings.html");
-
-  //Při pokusu o přístup na HTML, na které nemám práva
-  server.on("/*.html", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->redirect("/login.html");
-  });
+    //Po registraci odstranit handler
+    //server.removeHandler(firstRegistrationRedirectHandler);
+    //firstRegistrationRedirectHandler = NULL;
+    //server.removeHandler(firstRegistrationStaticHandler);
+    //firstRegistrationStaticHandler = NULL;
+  }
 
   //Default webpage
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->redirect("/orders.html");
   });
 
-  //ZDE API - rozhodnout se, zda aplikovat filtry, nebo kontrolovat až uvnitř
-    // server.on("/api/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
+  //Přihlášený uživatel
+  server.serveStatic("/orders.html", LittleFS, "/www/orders.html").setFilter(isAuthenticatedFilter);
+  server.serveStatic("/password-change.html", LittleFS, "/www/password-change.html").setFilter(isAuthenticatedFilter);
+
+  //Přihlášený s právy
+  server.serveStatic("/payment.html", LittleFS, "/www/payment.html").setFilter(isAuthenticatedFilter);
+
+  //Admin
+  server.serveStatic("/users.html", LittleFS, "/www/users.html").setFilter(isAuthenticatedFilter).setFilter(isAuthorizedAdminFilter);
+  server.serveStatic("/settings.html", LittleFS, "/www/settings.html").setFilter(isAuthenticatedFilter).setFilter(isAuthorizedAdminFilter);
+  ;
+
+  //Při pokusu o přístup na HTML, na které nemám práva
+  server.on("/*.html", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->redirect("/login.html");
+  });
+
+  //ZDE API - kontrouje se až uvnitř
+  // server.on("/api/heap", HTTP_GET, [](AsyncWebServerRequest *request) {
   //   request->send(200, "text/plain", String(ESP.getFreeHeap()));
   // });
 
@@ -80,22 +89,25 @@ void serverInit() {
 
 void setup() {
   Serial.begin(115200);
+
+  //NTP
   configTime(3600, 3600, "pool.ntp.org");
+
   if (!LittleFS.begin()) {
-    Serial.println("An Error has occurred while mounting LittleFS");
+    Serial.println("LittleFS Failed");
     return;
   }
 
   WiFi.mode(WIFI_STA);
   WiFi.begin("PaPaJ 2.4", "petapajajola");
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("WiFi Failed!\n");
+    Serial.println("WiFi Failed!");
     return;
   }
 
-  //čekání na NTP. max 5s;
   struct tm timeInfo;
   if (!getLocalTime(&timeInfo)) {
+    //čekání na NTP. max 5s;
     Serial.println("NTP Timeout");
   }
 
@@ -106,7 +118,5 @@ void setup() {
 
 void loop() {
   Serial.println(ESP.getFreeHeap());
-  //time_t current = time(nullptr);
-  //Serial.print(ctime(&current));
-  delay(5000);
+  delay(60 * 10000);
 }
